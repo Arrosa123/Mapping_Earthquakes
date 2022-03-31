37f1d19d68657767fa68c1e003c10fcfdeb7b5ef
Arrosa Qayyum <96403349+Arrosa123@users.noreply.github.com>
2022-03-31 00:00:20 -0700

    Update logic.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -31,7 +31,7 @@ cityData.forEach(function(city) {
 let streets =  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: 'pk.eyJ1IjoiYXJyb3NhMTIzIiwiYSI6ImNsMTFpb3VxazFheTYza28wd2tzcXpuMjIifQ.tSBa3Wxd3qIrvfV0l6rCDQ'
+    accessToken: API KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
