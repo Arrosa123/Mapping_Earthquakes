f2c7c06070e775f3456c60e3080ba8c3ab10c687
Your Name <you@example.com>
2022-03-30 23:02:59 -0700

    Add GeoJSON Map points

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,46 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map.
+let streets =  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: 'pk.eyJ1IjoiYXJyb3NhMTIzIiwiYSI6ImNsMTFpb3VxazFheTYza28wd2tzcXpuMjIifQ.tSBa3Wxd3qIrvfV0l6rCDQ'
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: 'pk.eyJ1IjoiYXJyb3NhMTIzIiwiYSI6ImNsMTFpb3VxazFheTYza28wd2tzcXpuMjIifQ.tSBa3Wxd3qIrvfV0l6rCDQ'
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/Arrosa123/Mapping_Earthquakes/main/majorAirports.json"
+ // Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data). addTo(map);
+});
